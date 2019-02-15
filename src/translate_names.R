201f7d353c9cfb549ea32dc785c20bab87d0299f
Niel Infante <aminfante@hsc.wvu.edu>
2019-02-15 11:09:58 -0500

    Add comments, write output

--- src/translate_names.R
+++ src/translate_names.R
@@ -1,42 +1,34 @@
 library(tidyverse)
 
+# Read Previously done DE
 d <- read.table('~/depot/projects/Kovinich/Kovinich_2019_01/previousWork/WGEvsH2O.txt',
 								header=T, sep="\t", stringsAsFactors = F)
 
 
+# Take only a portion of the glyma 1.1 ID
 d$GLYMA_1.1 <- substr(d$GLYMA_1.1, 7, 14)
-head(d)
 
-n <- read.table('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/WGE_NAC42_vs_pGWB2_results.txt',
-								header=T, sep="\t", stringsAsFactors = F)
-
-
-
-head(n)
-
+# Read the translation table
 trans <- read.table('~/depot/projects/Kovinich/Kovinich_2019_01/Data/Glyma_11_to_Glyma_20_Correspondence_Full.csv',
 									header=T, sep="\t", skip=2, stringsAsFactors = F)
 
 
-head(trans)
-#trans$Glyma.1.1 <- toupper(trans$Glyma.1.1)
-#trans$Glyma.1.1 <- sub('a','a.', trans$Glyma.1.1)
-#trans$Glyma.1.1 <- sub('g','G', trans$Glyma.1.1)
-#trans$Glyma.1.1 <- sub('s','S', trans$Glyma.1.1)
+#head(trans)
+
+# get IDs into usable form
 trans$Glyma.1.1 <- substr(trans$Glyma.1.1,6,99)
 trans$Glyma.1.1 <- toupper(trans$Glyma.1.1)
 
 trans$Glyma2.0 <- toupper(trans$Glyma2.0)
 trans$Glyma2.0 <- sub('[.]','_',trans$Glyma2.0)
 
-head(trans)
-substr(d[1:5,1], 1,15)
 
-
+# Join the IDs with the data table
 j <- inner_join(d, trans, by= c('GLYMA_1.1'='Glyma.1.1'))
 
 
+write_tsv(j,'~/depot/projects/Kovinich/Kovinich_2019_01/previousWork/withID_WGEvsH2O.txt')
 
-head(j)
 
-dim(j)
+
+
