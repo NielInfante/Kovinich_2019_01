b188dfc883d7f6e2d2697a58e9f0574ee05ca7cb
Niel Infante <aminfante@hsc.wvu.edu>
2019-02-18 15:19:12 -0500

    Make as function

--- src/annotate.R
+++ src/annotate.R
@@ -1,5 +1,6 @@
 # script to add some annotations to results files.
 
+library(tidyverse)
 library(AnnotationDbi)
 library(AnnotationHub)
 
@@ -7,30 +8,41 @@ hub <- AnnotationHub()
 query(hub, "Glycine max")
 gm <- hub[['AH66196']]
 
-columns(gm)
+#columns(gm)
 
 
 entrez <- read_tsv('~/depot/projects/Kovinich/Kovinich_2019_01/Data/entrez.tsv')
 entrez <- entrez %>% dplyr::select(GeneID=gene_stable_id, Entrez=xref)
 entrez$Entrez <- as.character(entrez$Entrez)
 
-
-res <- read_tsv('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/H2O_All_results.txt')
-
-res <- inner_join(res, entrez)
-
-
 my_concat <- function(x){paste(x, sep="|", collapse="|")}
 
 
-# Get gene names
-res$Symbol <- mapIds(gm, keys=res$Entrez, column='SYMBOL', keytype='ENTREZID', multiVals=my_concat)
-res$GeneName <- mapIds(gm, keys=res$Entrez, column='GENENAME', keytype='ENTREZID', multiVals=my_concat)
-res$Ontology <- mapIds(gm, keys=res$Entrez, column='ONTOLOGY', keytype='ENTREZID', multiVals=my_concat)
-res$OntologyAll <- mapIds(gm, keys=res$Entrez, column='ONTOLOGYALL', keytype='ENTREZID', multiVals=my_concat)
-res$GO <- mapIds(gm, keys=res$Entrez, column='GO', keytype='ENTREZID', multiVals=my_concat)
-res$GOAll <- mapIds(gm, keys=res$Entrez, column='GOALL', keytype='ENTREZID', multiVals=my_concat)
+doTheAnnotation('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/All_results.txt')
 
 
+doTheAnnotation('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/H2O_MYB29A2_vs_pGWB2_results.txt') 
+doTheAnnotation('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/WGE_MYB29A2_vs_pGWB2_results.txt')
+doTheAnnotation('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/H2O_All_results.txt')  
+doTheAnnotation('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/H2O_NAC42_vs_pGWB2_results.txt')    
+doTheAnnotation('~/depot/projects/Kovinich/Kovinich_2019_01/deseq/WGE_NAC42_vs_pGWB2_results.txt')
 
-write_tsv(res, '~/depot/projects/Kovinich/Kovinich_2019_01/deseq/H2O_All_results_annot.txt')
+
+# Define the Function
+doTheAnnotation <- function(fileName){
+
+	res <- read_tsv(fileName)
+
+	res <- inner_join(res, entrez)
+
+	# Get gene names
+	res$Symbol <- mapIds(gm, keys=res$Entrez, column='SYMBOL', keytype='ENTREZID', multiVals=my_concat)
+	res$GeneName <- mapIds(gm, keys=res$Entrez, column='GENENAME', keytype='ENTREZID', multiVals=my_concat)
+	res$Ontology <- mapIds(gm, keys=res$Entrez, column='ONTOLOGY', keytype='ENTREZID', multiVals=my_concat)
+	res$OntologyAll <- mapIds(gm, keys=res$Entrez, column='ONTOLOGYALL', keytype='ENTREZID', multiVals=my_concat)
+	res$GO <- mapIds(gm, keys=res$Entrez, column='GO', keytype='ENTREZID', multiVals=my_concat)
+	res$GOAll <- mapIds(gm, keys=res$Entrez, column='GOALL', keytype='ENTREZID', multiVals=my_concat)
+
+	
+	write_tsv(res, paste0(sub('\\.txt$', '', fileName), '_annot.txt'))
+}
